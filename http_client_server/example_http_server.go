69335f22076a08b4e67326c3d34c21fe161d38e3
Subrat Gyawali <subrat.gyawali@gmail.com>
2022-03-03 22:13:40 +0545

    http server

--- http_client_server/example_http_server.go
+++ http_client_server/example_http_server.go
@@ -2,11 +2,12 @@ package httpclientserver
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 func InitHttpServer() {
-	fmt.Println("Strting Web Server")
+	fmt.Println("Starting Web Server")
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.Write([]byte("Hello Golang Server"))
 	})
@@ -16,5 +17,21 @@ func InitHttpServer() {
 }
 
 func Hello(rw http.ResponseWriter, r *http.Request) {
-	rw.Write([]byte(`{"id":1,"name":"Ram"}`))
+	if r.Method == "GET" {
+		fmt.Println(r.URL.Query()["id"])
+		rw.Write([]byte(`{"id":1,"name":"Ram"}`))
+	} else if r.Method == "POST" {
+		fmt.Println(r.Header)
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(body))
+		rw.Write([]byte(`{"message":"successfully created data"}`))
+	} else if r.Method == "DELETE" {
+		rw.Write([]byte(`{"message":"successfully deleted data"}`))
+	} else if r.Method == "PUT" {
+		rw.Write([]byte(`{"message":"successfully updated data"}`))
+	}
+
 }
